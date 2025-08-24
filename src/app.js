a05fee8afc1592a6344692279ebac08e0944a6cf
Dev-Abhi1 <Dev-Abhi1>
2025-08-24 12:36:42 +0530

    index.html request fixed

--- src/app.js
+++ src/app.js
@@ -1,36 +1,30 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const path = require('path');
-
 
 /* Routes */
 const authRoutes = require('./routes/auth.routes');
 const chatRoutes = require("./routes/chat.routes");
 
-
 const app = express();
-
-
 
 /* using middlewares */
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173',  // for dev
     credentials: true
-}))
+}));
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
-
-
 
 /* Using Routes */
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
 
+// No need to serve index.html since frontend is on Netlify
 
-app.get("*name", (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get("/", (req, res) => {
+    res.send("Backend is running 🚀");
 });
 
 module.exports = app;
